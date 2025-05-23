d8e555b6d488542685737ce97c7c606b966484a5
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2025-05-23 11:05:10 +0500

    swagger added

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,29 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Golden Burger API",
+        default_version='v1',
+        description="API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="1.quvonchbek.ibragmov@gmail.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('orders.urls')),  # REST API routes
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # JWT obtain
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # JWT refresh
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+class UserRegistrationTest(APITestCase):
+    def test_user_registration_success(self):
+        data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "Testpass1234"
+        }
+        response = self.client.post(reverse('register'), data)
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('id', response.data)
